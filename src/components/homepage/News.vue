d0de69cda0ff2563aedfc485f8ade5ed483f6c27
Benyaporn54 <benyaporn.meat@mail.kmutt.ac.th>
2025-03-09 01:28:27 +0700

    054 - [add] Footer.vue, Announcement.vue and News.vue

---
+++ src/components/homepage/News.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+ <div class="flex ">
+     <div class="m-5">
+        <slot name="image" >
+            <img src="" alt="" class="w-full h-auto"/>
+        </slot>
+    </div>
+    
+    <div class="mt-2 w-full pt-2">
+
+      <slot name="title">
+        <h3 class="font-noto-sans-thai text-[#404546] text-2xl font-bold">Header</h3>
+      </slot>
+ 
+      <slot name="content">
+        <p>Content</p>
+      </slot>
+
+      <slot name="date">
+        <p>Date</p>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -1,13 +1,19 @@
 <script setup>
-import Banner from '../Banner.vue'
-import Reason from '../Reason.vue'
+import Banner from '../homepage/Banner.vue'
+import Reason from '../homepage/Reason.vue'
+import footers from "../homepage/Footer.vue"
+import announcement from "../homepage/Announcement.vue"
 </script>
  
 <template>
+
 <div>
     <Banner></Banner>
+    <announcement/>
     <Reason></Reason>
+    <footers/>
 </div>
+    
 </template>
  
 <style scoped>
