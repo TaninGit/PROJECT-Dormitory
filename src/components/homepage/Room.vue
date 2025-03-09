37b1ed82ba3d6ca58d5166c1e03a3af41b10751f
mook2005 <chananthorn.s@gmail.com>
2025-03-09 18:56:28 +0700

    018 - [add] roomtypes.json, Navbar.vue, Room.vue, Roomtype.vue and roomtypes images in public

---
+++ src/components/homepage/Room.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import roomTypes from '../../../data/roomtypes.json';
+import Roomtype from './Roomtype.vue';
+
+const rooms = ref([]);
+
+onMounted(() => {
+  rooms.value = roomTypes;
+});
+</script>
+
+<template>
+  <div class="relative bg-cover bg-center py-16 w-full bg-[url('../assets/background/roomcomponentBackground.png')]">
+    <div class="absolute inset-0 bg-black opacity-20"></div>
+
+    <div class="relative z-10 flex flex-wrap justify-center gap-12 mt-12">
+      <div class="relative z-10 w-64 flex flex-col justify-center items-center text-white font-noto-sans-thai drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]">
+        <h1 class="font-bold text-6xl">ห้องพัก</h1>
+        <div class="bg-[#E09F3E] h-1 w-40 mx-auto mt-2"></div>
+      </div>
+      <Roomtype v-for="room in rooms" :key="room.id">
+        <template #image>
+          <img :src="`/roomtypes/${room.image}`" alt="รูปห้องพัก" class="w-full h-full object-cover">
+        </template>
+        <template #title>
+          {{ room.title }}
+        </template>
+        <template #description>
+          {{ room.description }}
+        </template>
+      </Roomtype>
+    </div>
+  </div>
+</template>
---
+++ src/components/homepage/Roomtype.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-[#1E555C] text-white p-6 rounded-lg drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] w-[350px] flex flex-col items-center font-noto-sans-thai">
+    <div class="w-24 h-24 rounded-full overflow-hidden mb-4">
+      <slot name="image">รูปภาพ</slot>
+    </div>
+    <h3 class="text-2xl font-bold text-[#E09F3E] mb-2">
+      <slot name="title">ชื่อห้อง</slot>
+    </h3>
+    <p class="text-sm text-center mb-6 w-60">
+      <slot name="description">คำอธิบายห้อง</slot>
+    </p>
+    <button class="mt-auto bg-[#E09F3E] hover:bg-[#9db5b2] text-white py-2 px-4 rounded-lg w-full cursor-pointer">
+      รายละเอียดห้องพัก
+    </button>
+  </div>
+</template>
