9fb7e1159bdb40ecdeb218110eb76f7e751d4f78
CNProud <proud1692916939@gmail.com>
2025-03-14 17:54:47 +0700

    021 - [add] deleteReport, confirmDelete function in RepairList.vue and edit Dropdown.vue(add toggleDropdown, selectItem function), Input.vue and edit db.json

--- src/components/form/Dropdown.vue
+++ src/components/form/Dropdown.vue
@@ -2,48 +2,53 @@
 import { ref } from 'vue'
 
 const props = defineProps({
-    label: String,
-    list: Array,
-    placeholder: {
-        type: String,
-        default: "โปรดเลือก"
-    }
-})
-const selectedValue = ref("") 
-const showError = ref(false)
-
-const validateInput = () => {
-  if (selectedValue.value === "") {
-    showError.value = true 
-  } else {
-    showError.value = false
+  label: String,
+  list: Array,
+  placeholder: {
+    type: String,
+    default: "โปรดเลือก"
   }
-}
-</script>
- 
-<template>
-<div>
-  <div class="mb-4">
-    <label class="block text-[#1E555C] text-lg font-noto-sans-thai">{{ label }}<span class="text-[#E09F3E]">*</span></label>
-    <div class="relative w-[390px]">
-        <select
-            v-model="selectedValue"
-            class="block w-[390px] appearance-none text-lg text-[#404546] bg-white border border-[#404546] rounded-full pl-4 pr-10 py-2"
-            @change="validateInput">
-            <option value="" disabled selected
-            class="text-[#404546] font-noto-sans-thai">
-            {{ placeholder }}</option>
-            <option class="text-[#404546] font-noto-sans-thai" v-for="(item, index) in list" :key="index" :value="item">{{ item }}</option>
-        </select> 
-        <div class="mr-[0.8px] absolute right-0 top-1/2 transform -translate-y-1/2 pointer-events-none bg-[#E09F3E] w-13 h-11 flex items-center justify-center rounded-r-full">
-            <img src="../../assets/icon/arrow.png" alt="dropdown arrow" class="w-4 h-3" />
-        </div>  
-    </div>   
-    <p v-if="showError" class="mt-2 text-md text-red-600 font-noto-sans-thai">กรุณาใส่ข้อมูล</p>
-  </div>
-</div>
-</template>
- 
-<style scoped>
+})
+const selectedValue = ref("")
+const openDropdown = ref(false);
 
-</style>
+const toggleDropdown = () => {
+  openDropdown.value = !openDropdown.value;
+};
+const selectItem = (item) => {
+  selectedValue.value = item
+  openDropdown.value = false
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="mb-4">
+      <label class="block text-[#1E555C] text-lg font-noto-sans-thai">{{ label }}<span
+          class="text-[#E09F3E]">*</span></label>
+      <div class="relative w-[390px]">
+        <div @click="toggleDropdown"
+          class="block w-[390px] appearance-none text-lg text-[#404546] bg-white border-1 border-[#404546] rounded-full pl-4 pr-10 py-[7.5px] cursor-pointer">
+          <span class="font-noto-sans-thai">{{
+            selectedValue || placeholder
+          }}</span>
+          <div
+            class="mr-[0.8px] absolute right-0 top-1/2 transform -translate-y-1/2 pointer-events-none bg-[#E09F3E] w-13 h-[45px] flex items-center justify-center rounded-r-full">
+            <img src="../../assets/icon/arrow.png" alt="dropdown arrow" class="w-4 h-3" />
+          </div>
+        </div>
+      </div>
+
+      <ul v-if="openDropdown"
+        class=" mt-1 bg-white border border-[#404546] rounded-2xl shadow-lg max-h-60 w-[390px] absolute z-10">
+        <li v-for="(item, index) in list" :key="index" @click="selectItem(item)"
+          class="px-4 py-2 cursor-pointer text-black font-noto-sans-thai rounded-2xl hover:bg-[#9DB5B2]/50">
+          {{ item }}
+          <hr v-if="index !== list.length - 1" class="text-[#9DB5B2] mt-1">
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped></style>
--- src/components/repair/RepairList.vue
+++ src/components/repair/RepairList.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { getItems } from "../../libs/fetchUtils.js";
+import { getItems, getItemById, editItem } from "../../libs/fetchUtils.js";
 import { ref, onMounted } from "vue";
 const userRepair = ref([]);
 const repairInfo = ref([]);
 const currentStudentId = "66130500054"; // studentId
 const userProfile = ref([]);
 const userInfo = ref([]);
+const showDeleteConfirm = ref(false);
+const selectedReportId = ref(null);
 
 onMounted(async () => {
   try {
@@ -13,7 +15,7 @@ onMounted(async () => {
       `${import.meta.env.VITE_APP_URL}/repairs`
     );
     const user = userRepair.value.find(
-      (user) => user.studentId === currentStudentId
+      (user) => user.id === currentStudentId
     );
     if (user) {
       repairInfo.value = user;
@@ -68,148 +70,148 @@ const handleEdit = (reportId) => {
   console.log(`Edit reportId: ${reportId}`);
 };
 
-const handleCancel = (reportId) => {
-  alert(`ยกเลิกรายการแจ้งซ่อม ID: ${reportId}`);
-  console.log(`Cancle reportId: ${reportId}`);
+const confirmDelete = (reportId) => {
+  selectedReportId.value = reportId;
+  showDeleteConfirm.value = true;
 };
+
+const deleteReport = async (studentId, repairId) => {
+  if (!selectedReportId.value) return;
+  try {
+    const student = await getItemById(`${import.meta.env.VITE_APP_URL}/repairs`, studentId)
+
+    if (student && student.reports) {
+      const index = student.reports.findIndex(report => report.repairId === repairId);
+      if (index !== -1) {
+        student.reports.splice(index, 1);
+        repairInfo.value.reports = student.reports
+        await editItem(`${import.meta.env.VITE_APP_URL}/repairs`, student.id, student)
+        if (expended.value === index) expended.value = null
+      }
+    }
+  } catch (error) {
+    console.error('เกิดข้อผิดพลาด:', error);
+  }
+  showDeleteConfirm.value = false;
+  selectedReportId.value = null;
+}
+
 </script>
 
 <template>
   <div class="font-noto-sans-thai">
-    <div
-      class="w-screen h-screen flex item-center justify-center"
+    <div class="w-screen h-screen flex item-center justify-center"
       style="background-image: url('src/assets/background/dormBackground.png')">
       <div class="pt-25 flex flex-col item-center justify-center items-center">
         <div class="w-350 h-25 flex gap-5">
-          <button
-            type="button"
-            class="flex items-center justify-center cursor-pointer">
-            <img
-              src="../../assets/icon/arrow-back.png"
-              alt="back-arrow"
-              class="absolute w-14"/>
-            <img
-              src="../../assets/icon/button-back.png"
-              alt="back-button"
-              class="w-22"/>
+          <button type="button" class="flex items-center justify-center cursor-pointer">
+            <img src="../../assets/icon/arrow-back.png" alt="back-arrow" class="absolute w-14" />
+            <img src="../../assets/icon/button-back.png" alt="back-button" class="w-22" />
           </button>
           <h1
-            class="bg-[#ffffff] border-3 border-[#E09F3E] rounded-3xl mt-2 mb-2 px-15 text-4xl text-center flex items-center justify-center font-noto-sans-thai font-medium">
+            class="bg-[#ffffff] border-3 border-[#E09F3E] text-[#1E555C] rounded-3xl mt-2 mb-2 px-15 text-4xl text-center flex items-center justify-center font-noto-sans-thai font-bold">
             บริการแจ้งซ่อมออนไลน์
           </h1>
           <button
             class="bg-[#404546] hover:bg-[#313637] text-white font-light mt-4 mb-4 px-10 rounded-xl font-noto-sans-thai ml-113 cursor-pointer transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
-            <img
-              src="/src/assets/icon/externalLinkIcon.png"
-              alt="externalLinkIcon"
-              class="inline w-5 h-5 mr-1"/>
+            <img src="/src/assets/icon/externalLinkIcon.png" alt="externalLinkIcon" class="inline w-5 h-5 mr-1" />
             <span>กรอกฟอร์มแจ้งซ่อมแซม</span>
           </button>
         </div>
         <div
           class="bg-[#ffffff] border-3 border-[#E09F3E] rounded-3xl w-300 h-120 overflow-y-auto [&::-webkit-scrollbar]:w-2 dark:[&::-webkit-scrollbar-track]:bg-[#ffffff] dark:[&::-webkit-scrollbar-track]:hidden dark:[&::-webkit-scrollbar-thumb]:bg-[#D49029] [&::-webkit-scrollbar-thumb]:rounded-full">
           <div class="bg-[#E09F3E] p-4">
-            <div
-              class="grid grid-cols-4 text-center text-[#ffffff] font-noto-sans-thai font-bold">
+            <div class="grid grid-cols-4 text-center text-[#ffffff] font-noto-sans-thai font-bold">
               <div>เวลา</div>
               <div>สถานะ</div>
               <div>รายละเอียดการซ่อมแซม</div>
               <div>ประเภทงานซ่อม</div>
             </div>
           </div>
-          <div
-            v-for="(report, index) in repairInfo?.reports"
-            :key="index"
-            class="flex flex-col justify-center items-center text-center"
-          >
+          <div v-for="(report, index) in repairInfo?.reports" :key="index"
+            class="flex flex-col justify-center items-center text-center">
             <div class="pt-4 pb-4">
-              <div
-                :class="[
-                  'cursor-pointer hover:bg-[#FFF4E3] border-[#E09F3E] font-noto-sans-thai grid grid-cols-4 gap-4 items-center border-3 rounded-2xl w-285 h-20 transition delay-20 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100',
-                  { 'bg-[#FFF4E3]': expended === index },
-                ]"
-                @click="toggleDetails(index)"
-              >
+              <div :class="[
+                'cursor-pointer hover:bg-[#FFF4E3] border-[#E09F3E] font-noto-sans-thai grid grid-cols-4 gap-4 items-center border-3 rounded-2xl w-285 h-20 transition delay-20 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100',
+                { 'bg-[#FFF4E3]': expended === index },
+              ]" @click="toggleDetails(index)">
                 <div>{{ report.reportTime }}</div>
                 <div :class="getStatusColor(report.status)" class="font-medium">
-                  <img
-                    v-if="report.status === 'รอดำเนินการ'"
-                    :src="getProgressIcon(report.status)"
-                    alt="icon"
-                    class="inline w-4 h-4 mr-2"
-                  />
-                  <img
-                    v-else-if="report.status === 'กำลังดำเนินการ'"
-                    :src="getProgressIcon(report.status)"
-                    alt="icon"
-                    class="inline w-4 h-4 mr-2"
-                  />
-                  <img
-                    v-else-if="report.status === 'ดำเนินการเรียบร้อย'"
-                    :src="getProgressIcon(report.status)"
-                    alt="icon"
-                    class="inline w-4 h-4 mr-2"
-                  />
+                  <img v-if="report.status === 'รอดำเนินการ'" :src="getProgressIcon(report.status)" alt="icon"
+                    class="inline w-4 h-4 mr-2" />
+                  <img v-else-if="report.status === 'กำลังดำเนินการ'" :src="getProgressIcon(report.status)" alt="icon"
+                    class="inline w-4 h-4 mr-2" />
+                  <img v-else-if="report.status === 'ดำเนินการเรียบร้อย'" :src="getProgressIcon(report.status)"
+                    alt="icon" class="inline w-4 h-4 mr-2" />
                   {{ report.status }}
                 </div>
                 <div class="truncate">{{ report.title }}</div>
                 <div>{{ report.repairType }}</div>
               </div>
               <transition name="drop">
-                <div
-                  v-if="expended === index"
-                  class="bg-[#ffffff] border-4 border-[#E09F3E] rounded-2xl w-285 p-4 text-left flex flex-col"
-                >
+                <div v-if="expended === index"
+                  class="bg-[#ffffff] border-4 border-[#E09F3E] text-[#1E555C] rounded-2xl w-285 p-4 text-left flex flex-col">
                   <div>
                     <p>
-                      <strong>รายละเอียดเพิ่มเติม:</strong> {{ report.title }}
+                      <strong>รายละเอียดเพิ่มเติม :</strong> {{ report.title }}
                     </p>
                   </div>
                   <div class="flex space-x-42">
                     <div>
                       <p>
-                        <strong>ผู้แจ้ง:</strong> {{ userProfile.firstName }}
+                        <strong>ผู้แจ้ง :</strong> {{ userProfile.firstName }}
                         {{ userProfile.lastName }}
                       </p>
                       <p>
-                        <strong>หอพัก:</strong>
+                        <strong>หอพัก :</strong>
                         <span v-if="userProfile?.gender === 'female'">
-                          หญิง</span
-                        >
+                          หญิง</span>
                         <span v-else-if="userProfile?.gender === 'male'">
-                          ชาย</span
-                        >
+                          ชาย</span>
                       </p>
-                      <p><strong>ห้อง:</strong> {{ userProfile.roomID }}</p>
+                      <p><strong>ห้อง :</strong> {{ userProfile.roomID }}</p>
                     </div>
                     <div>
                       <p>
-                        <strong>เบอร์โทรศัพท์:</strong> {{ userProfile.phone }}
+                        <strong>เบอร์โทรศัพท์ :</strong> {{ userProfile.phone }}
                       </p>
-                      <p><strong>อีเมล:</strong> {{ userProfile.email }}</p>
+                      <p><strong>อีเมล :</strong> {{ userProfile.email }}</p>
                       <p>
-                        <strong>วันและเวลานัดหมาย:</strong>
+                        <strong>วันและเวลานัดหมาย :</strong>
                         {{ report.appointmentDate }} เวลา
                         {{ report.appointmentTime }}
                       </p>
                     </div>
                     <div>
-                      <div
-                        v-if="report?.status === 'รอดำเนินการ'"
-                        class="flex flex-col gap-1"
-                      >
+                      <div v-if="report?.status === 'รอดำเนินการ'" class="flex flex-col gap-1">
                         <button
                           class="bg-[#ffffff] hover:bg-[#EDEDED] border-3 border-[#404546] px-4 py-2 rounded-md cursor-pointer"
-                          @click="handleEdit(report.repairId)"
-                        >
+                          @click="handleEdit(report.repairId)">
                           แก้ไข
                         </button>
                         <button
                           class="bg-[#ffffff] hover:bg-[#EDEDED] border-3 border-[#F1402C] px-4 py-2 rounded-md cursor-pointer"
-                          @click="handleCancel(report.repairId)"
-                        >
+                          @click="confirmDelete(report.repairId)">
                           ยกเลิก
                         </button>
+                      </div>
+                      <div v-if="showDeleteConfirm"
+                        class="fixed inset-0 flex items-center justify-center backdrop-blur-xs">
+                        <div class="bg-white p-6 rounded-xl shadow-lg w-96 text-center animate-fade-in">
+                          <h2 class="text-lg font-semibold text-gray-800">ยืนยันการลบ</h2>
+                          <p class="text-gray-600 mt-2">คุณแน่ใจหรือไม่ว่าต้องการยกเลิกการแจ้งซ่อมนี้?</p>
+
+                          <div class="mt-4 flex justify-center gap-4">
+                            <button @click="deleteReport(currentStudentId, report.repairId)"
+                              class="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">
+                              ยืนยัน
+                            </button>
+                            <button @click="showDeleteConfirm = false"
+                              class="px-4 py-2 bg-gray-300 text-gray-800 rounded-lg hover:bg-gray-400 ">
+                              ยกเลิก
+                            </button>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -231,16 +233,19 @@ const handleCancel = (reportId) => {
   max-width: 200px;
 }
 
-.drop-enter-active, .drop-leave-active {
+.drop-enter-active,
+.drop-leave-active {
   transition: all 0.3s ease-in-out;
 }
 
-.drop-enter-from, .drop-leave-to {
+.drop-enter-from,
+.drop-leave-to {
   opacity: 0;
   transform: scale(0.8);
 }
 
-.drop-enter-to, .drop-leave-from {
+.drop-enter-to,
+.drop-leave-from {
   opacity: 1;
   transform: scale(1);
 }
