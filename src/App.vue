4807b42bf6750b58151cf95f461fd6df03016ce0
Benyaporn54 <benyaporn.meat@mail.kmutt.ac.th>
2025-03-15 17:35:35 +0700

    054 - [add] Profile.vue and UpdateRepairForm.vue

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@ import Repair from "./components/pages/Repair.vue"
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 import AddRepairForm from "./components/repair/AddRepairForm.vue";
+import UpdateRepairForm from "./components/repair/UpdateRepairForm.vue";
+import RepairList from "./components/repair/RepairList.vue";
 </script>
 
 <template>
     <Navbar />
-    <AddRepairForm></AddRepairForm>
+    <RepairList></RepairList>
+    <!-- <UpdateRepairForm></UpdateRepairForm> -->
     <Footer></Footer>
 </template>
 
